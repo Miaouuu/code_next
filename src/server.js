f3734d479b8df87c175b69fb480d2686c7a15df0
Guillaume PRIGENT <guillaume.prigent92@gmail.com>
2021-03-05 22:55:33 +0100

    🚧Netlify 3rd attempt

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // src/server.js
 
 const express = require("express")();
-const server = require("http").Server(express);
+const server = require("https").Server(express);
 const io = require("socket.io")(server);
 const next = require("next");
 
@@ -10,24 +10,15 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const {
-  createRoom,
-  joinRoom,
-  checkRoom,
-  sendCode,
-  getResult,
-  sendSolution
-} = require("./controller");
+const { createRoom, joinRoom, checkRoom, sendCode, getResult, sendSolution } = require("./controller");
 
-io.on("connect", (socket) => {
-  socket.on("CREATE_ROOM", (type) => createRoom(type, socket));
-  socket.on("JOIN_ROOM", (pin) => joinRoom(pin, socket));
-  socket.on("CHECK_ROOM", (pin) => checkRoom(pin, socket));
-  socket.on("SEND_CODE", ({ type, code }) =>
-    sendCode(type, code, socket.id, io)
-  );
-  socket.on("GET_RESULT", (pin) => getResult(pin, socket));
-  socket.on("SEND_RESOLUTION", ({order, pin}) => sendSolution(order, pin, io))
+io.on("connect", socket => {
+  socket.on("CREATE_ROOM", type => createRoom(type, socket));
+  socket.on("JOIN_ROOM", pin => joinRoom(pin, socket));
+  socket.on("CHECK_ROOM", pin => checkRoom(pin, socket));
+  socket.on("SEND_CODE", ({ type, code }) => sendCode(type, code, socket.id, io));
+  socket.on("GET_RESULT", pin => getResult(pin, socket));
+  socket.on("SEND_RESOLUTION", ({ order, pin }) => sendSolution(order, pin, io));
 });
 
 app.prepare().then(() => {
@@ -35,7 +26,7 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(port, (err) => {
+  server.listen(port, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
