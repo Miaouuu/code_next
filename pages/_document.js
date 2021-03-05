b7ef9189e7c73b1d9fa5bde7afb682e5ec3c75d1
Guillaume PRIGENT <guillaume.prigent92@gmail.com>
2021-03-05 21:37:56 +0100

    🔍 Improved og content

--- pages/_document.js
+++ pages/_document.js
@@ -12,16 +12,16 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta property="og:title" content="JPO" key="title" />
+          <meta property="og:title" content="JPO IIM" key="title" />
 
           <meta name="description" content="Site de la JPO du 06-03-2021" />
           <meta property="og:description" content="Site de la JPO du 06-03-2021" key="description" />
 
-          <meta property="og:url" content="#" key="url" />
+          <meta property="og:url" content="https://code-next.herokuapp.com/" key="url" />
           <meta property="og:type" content="website" key="type" />
 
-          <link rel="icon" href="/images/favicon-placeholder.png" />
-          <meta property="og:image" content="/images/favicon-placeholder.png" key="image" />
+          <link rel="icon" href="/images/iim-logo.jpg" />
+          <meta property="og:image" content="/images/iim-logo.jpg" key="image" />
 
           <link href="/css/style.css" rel="stylesheet" />
         </Head>
